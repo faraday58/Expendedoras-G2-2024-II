e2a20750ee7015fadf824685e82fbac1bfb1d9b0
armando <jarodriguezvera@gmail.com>
2024-02-12 12:46:40 -0600

    Add comentario

--- Cafe.cs
+++ Cafe.cs
@@ -4,6 +4,7 @@ namespace Expendedoras_G2_2024_II
 {
     internal class Cafe:Expendedora
     {
+        //Comentario
         public Cafe()
         {
             Console.WriteLine("Que tal un delicioso Café");
